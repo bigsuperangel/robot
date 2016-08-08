42930dfff3f5b85f25544748f6a595bf2ced774b
bigsuperyu <bigsuperangel@gmail.com>
2016-08-08 13:43:01 +0800

    更改httputil方式

--- src/main/java/com/fj/robot/Robot.java
+++ src/main/java/com/fj/robot/Robot.java
@@ -3,6 +3,7 @@ package com.fj.robot;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.fj.hiwetoptools.web.HttpUtil;
+import sun.security.krb5.internal.crypto.Aes128CtsHmacSha1EType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,13 +13,10 @@ import java.util.Map;
  */
 public class Robot {
     private static final String ITPK_API = "http://i.itpk.cn/api.php";  //茉莉机器人
-    // 这里的api_key和api_secret可以自己申请一个
-    private static final String KEY = "?api_key=13b52e0033109dd0e3a17570a63b9395&api_secret=rgif59isberr";
 
-    private static final String TULING_API = "http://www.tuling123.com/openapi/api?key=6cf833817aab217eca8bf182b63270c0&info=";
+    private static final String TULING_API = "http://www.tuling123.com/openapi/api";
 
     public static String xiaodoubi(String msg) {
-        String url = ITPK_API + KEY + "&question=" + msg;
         Map<String,String> map = new HashMap<>();
         map.put("api_key","13b52e0033109dd0e3a17570a63b9395");
         map.put("api_secret","rgif59isberr");
@@ -29,8 +27,11 @@ public class Robot {
     }
 
     public static String tuling(String msg,String userId){
-        String url = TULING_API + msg + "&userId="+userId;
-        String result =  HttpUtil.get(url);
+        Map map = new HashMap();
+        map.put("key", "6cf833817aab217eca8bf182b63270c0");
+        map.put("info", msg);
+        map.put("userId",userId);
+        String result =  HttpUtil.get(TULING_API,map);
         JSONObject j = JSON.parseObject(result);
         return j.getString("text");
     }
